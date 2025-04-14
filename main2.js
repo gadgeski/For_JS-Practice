6a9722450691b85609a8154b96ec4b411cd55ba4
gadgeski <xtwh.ce5bil@gmail.com>
2025-04-14 20:43:25 +0900

    半角と全角の○の順序を逆にしてみる

---
+++ main2.js
@@ -0,0 +1,121 @@
+// ゲームの状態
+const CONTINUE = null; // 決着がついてない
+const WIN_PLAYER_1 = 1; // ○の勝利
+const WIN_PLAYER_2 = -1; // xの勝利
+const DRAW_GAME = 0; // 引き分け
+
+const cells =[ // カラなら０、　○なら1, xなら-1
+    // 二次元配列を記述
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+
+]
+let turn = 1; // ○なら１、　xなら-1
+let result = CONTINUE;
+
+// セルをクリックしたときのイベント登録
+for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+        const cell = document.querySelector(`#cell_${row}_${col}`);
+        cell.addEventListener("click", () => {
+            if (result !== CONTINUE) {
+                window.location.reload(true); // 決着がついた後にクリックしてリロード
+            }
+            if (cells[row][col] === 0) { // 配置可能か判定
+            putMark(row, col);　// ○かxを配置
+            turn = turn * -1;
+            thinkAI(); // AIに考えてもらう
+            turn = turn * -1;
+            check(); // ゲームの状態を確認
+            }
+        });
+    }
+}
+
+// ○か×を配置
+function putMark(row,col) {
+    const cell = document.querySelector(`#cell_${row}_${col}`);
+    if (turn === 1) {
+        // 半角と全角を逆にすると、表示はどうなるか？
+        cell.textContent = "o";
+        cell.classList.add("◯");
+        cells[row][col] = 1;
+    } else {
+        cell.textContent = "x";
+        cell.classList.add("x");
+        cells[row][col] = -1;
+    }
+}
+
+// ゲームの状態を確認
+function check() {
+    result = judge(cells);
+    const message = document.querySelector("#message");
+    switch (result) {
+        case WIN_PLAYER_1:
+            message.textContent = "○の勝ち!";
+            // message_textContentをmessage.textContentに修正
+            break;
+        case WIN_PLAYER_2:
+            message.textContent = "xの勝ち!";
+            // message_textContentをmessage.textContentに修正
+            break;
+        case DRAW_GAME:
+            message.textContent = "引き分け!";
+            // message_textContentをmessage.textContentに修正
+            break;
+
+    }
+    
+}
+
+// 勝敗を判定する処理
+function judge(_cells) {
+    // 調べるマス目をラインナップ
+    const lines = [
+        // 横マスをチェック
+        [_cells[0][0], _cells[0][1], _cells[0][2]],
+        [_cells[1][0], _cells[1][1], _cells[1][2]],
+        [_cells[2][0], _cells[2][1], _cells[2][2]],
+        // 縦マスをチェック
+        [_cells[0][0], _cells[1][0], _cells[2][0]],
+        [_cells[0][1], _cells[1][1], _cells[2][1]],
+        [_cells[0][2], _cells[1][2], _cells[2][2]],
+        // 斜めマスをチェック
+        [_cells[0][0], _cells[1][1], _cells[2][2]],
+        [_cells[0][2], _cells[1][1], _cells[2][0]],
+    ];
+    // 勝敗チェック
+    for (let line of lines) {
+        const sum = line[0] + line[1] + line[2];
+        if (sum === 3) {
+            return WIN_PLAYER_1;
+        }
+        if (sum === -3) {
+            return WIN_PLAYER_2;
+        }
+    }
+    // 引き分けチェック
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (_cells[row][col] === 0) {
+                return CONTINUE;
+            }
+        }
+    }
+    return DRAW_GAME;
+
+}
+
+// AIに考えてもらう
+function thinkAI() {
+    const hand = think(cells, -1, 5);
+    if (hand) {
+        const cell = document.querySelector(`#cell_${hand[0]}_${hand[1]}`); // テンプレートリテラルを修正
+        cell.textContent ="x";
+        cell.classList.add("x");
+        cells[hand[0]][hand[1]] = -1;
+        // -1,を-1;に修正
+    }
+}
